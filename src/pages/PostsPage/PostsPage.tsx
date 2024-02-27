1e8fb0f08dac5c37caa04f9ab3553f72b5b3523b
vladislav <rozovvladislav76@yandex.ru>
2024-02-27 18:07:40 +0300

    Исправил экспорт

--- src/pages/PostsPage/PostsPage.tsx
+++ src/pages/PostsPage/PostsPage.tsx
@@ -2,7 +2,7 @@ import PostsList from "../../components/PostsList/PostsList";
 import styles from "./styles.module.scss";
 import { FC } from "react";
 
-export const PostsPage: FC = () => {
+const PostsPage: FC = () => {
 	return (
 		<div className={styles.root}>
 			<h1>Posts</h1>
